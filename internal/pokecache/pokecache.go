e2e9ec6a39e70aa594dd98975123be936f7e06e7
nmema <nicolas.a.mema@gmail.com>
2023-08-12 20:49:19 -0300

    #9 add mux

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -1,9 +1,13 @@
 package pokecache
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 type Cache struct {
 	cache map[string]cacheEntry
+	mux   *sync.Mutex
 }
 
 type cacheEntry struct {
@@ -14,12 +18,15 @@ type cacheEntry struct {
 func NewCache(interval time.Duration) Cache {
 	c := Cache{
 		cache: make(map[string]cacheEntry),
+		mux:   &sync.Mutex{},
 	}
 	go c.reapLoop(interval)
 	return c
 }
 
 func (c *Cache) Add(key string, value []byte) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
 	c.cache[key] = cacheEntry{
 		value:     value,
 		createdAt: time.Now().UTC(),
@@ -27,6 +34,8 @@ func (c *Cache) Add(key string, value []byte) {
 }
 
 func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
 	entry, ok := c.cache[key]
 	return entry.value, ok
 }
@@ -39,6 +48,8 @@ func (c *Cache) reapLoop(interval time.Duration) {
 }
 
 func (c *Cache) reap(interval time.Duration) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
 	timeAgo := time.Now().UTC().Add(-interval)
 	for key, value := range c.cache {
 		if value.createdAt.Before(timeAgo) {
