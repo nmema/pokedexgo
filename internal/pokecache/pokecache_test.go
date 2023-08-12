2c4d74ef00353d5923ee246afd1aa1a359abcf24
nmema <nicolas.a.mema@gmail.com>
2023-08-12 18:22:22 -0300

    #5 add reap

--- internal/pokecache/pokecache_test.go
+++ internal/pokecache/pokecache_test.go
@@ -2,17 +2,18 @@ package pokecache
 
 import (
 	"testing"
+	"time"
 )
 
 func TestCreateCache(t *testing.T) {
-	cache := NewCache()
+	cache := NewCache(time.Millisecond * 10)
 	if cache.cache == nil {
 		t.Error("cache is nil")
 	}
 }
 
 func TestAddGetCache(t *testing.T) {
-	cache := NewCache()
+	cache := NewCache(time.Millisecond * 10)
 
 	cases := []struct {
 		inputKey string
@@ -43,5 +44,35 @@ func TestAddGetCache(t *testing.T) {
 		if string(actual) != string(cs.inputVal) {
 			t.Errorf("%s does not match %s", string(actual), string(cs.inputVal))
 		}
+	}
+}
+
+func TestReapOk(t *testing.T) {
+	interval := time.Millisecond * 10
+	cache := NewCache(interval)
+
+	keyOne := "foo"
+	cache.Add(keyOne, []byte("bar"))
+
+	time.Sleep(interval + time.Millisecond*10)
+
+	_, ok := cache.Get(keyOne)
+	if ok {
+		t.Errorf("%s should have been reaped", keyOne)
+	}
+}
+
+func TestReapFailed(t *testing.T) {
+	interval := time.Millisecond * 10
+	cache := NewCache(interval)
+
+	keyOne := "foo"
+	cache.Add(keyOne, []byte("bar"))
+
+	time.Sleep(interval / 2)
+
+	_, ok := cache.Get(keyOne)
+	if !ok {
+		t.Errorf("%s shouldn't have been reaped", keyOne)
 	}
 }
