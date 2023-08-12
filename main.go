5b8835b3c12e45c9d3f01cca1f97fa12ec480c7c
nmema <nicolas.a.mema@gmail.com>
2023-08-12 20:43:49 -0300

    #9 implement catch command

--- main.go
+++ main.go
@@ -10,11 +10,13 @@ type config struct {
 	pokeapiClient pokeapi.Client
 	Next          *string
 	Previous      *string
+	pokemonCaught map[string]pokeapi.PokemonInformation
 }
 
 func main() {
 	config := config{
 		pokeapiClient: pokeapi.NewClient(time.Hour),
+		pokemonCaught: make(map[string]pokeapi.PokemonInformation),
 	}
 
 	startRelp(&config)
