84489f4bb167425d68380a8c4dcf9209711e38a1
nmema <nicolas.a.mema@gmail.com>
2023-08-12 02:50:46 -0300

    #1 add help command

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func()
+}
+
+func commandHelp() {
+	fmt.Println("")
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+
+	commands := getCommands()
+
+	for _, command := range commands {
+		fmt.Printf("%s:  %s", command.name, command.description)
+	}
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
+var commands map[string]cliCommand = getCommands()
 
 func main() {
-	fmt.Println("Hello, world!")
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Pokedex > ")
+	scanner.Scan()
+	text := scanner.Text()
+
+	fmt.Println(text)
+	commands[text].callback()
+
+	// commands[text].callback()
 }
